d148e1d52079de4f9c2f16690e908acd79df9994
egamberdiyevxushnud <xushnudegamberdiyev0@gmail.com>
2024-03-13 14:44:48 +0500

    theory

--- Test/Calculator.Test/CalculatorTests.cs
+++ Test/Calculator.Test/CalculatorTests.cs
@@ -8,42 +8,54 @@ namespace Calculator.Test
 {
     public class CalculatorTests
     {
-        [Fact]
-        public void AddTest()
+        [Theory]
+        [InlineData(1,2,3)]
+        [InlineData(4,5,9)]
+        [InlineData(7,8,15)]
+        public void AddTest(double a, double b, double res)
         {
             Calculator calculator = new Calculator();
 
-            var result = calculator.add(2, 3);
+            var result = calculator.add(a,b);
 
-            Assert.Equal(5, result);
+            Assert.Equal(res, result);
 
         }
-        [Fact]
-        public void Subtract()
+        [Theory]
+        [InlineData(10,5,5)]
+        [InlineData(11,10,1)]
+        [InlineData(16,11,5)]
+        public void Subtract(double a, double b, double res)
         {
             Calculator calculator = new Calculator();
 
-            var result = calculator.Subtract(10, 5);
+            var result = calculator.Subtract(a,b);
 
-            Assert.Equal(5, result);
+            Assert.Equal(res, result);
         }
-        [Fact]
-        public void Multiply()
+        [Theory]
+        [InlineData(2,5,10)]
+        [InlineData(3,5,15)]
+        [InlineData(8,2,16)]
+        public void Multiply(double a, double b, double res)
         {
             Calculator calculator = new Calculator();
 
-            var result = calculator.Multiply(2, 3);
+            var result = calculator.Multiply(a,b);
 
-            Assert.Equal(6, result);
+            Assert.Equal(res, result);
         }
-        [Fact] 
-        public void Divide()
+        [Theory]
+        [InlineData(10,5,2)]
+        [InlineData(40,10,4)]
+        [InlineData(45,3,15)]
+        public void Divide(double a, double b, double res)
         {
             Calculator calculator = new Calculator();
 
-            var result = calculator.Divide(10, 5);
+            var result = calculator.Divide(a,b);
 
-            Assert.Equal(2,result);
+            Assert.Equal(res ,result);
         }
     }
 }
